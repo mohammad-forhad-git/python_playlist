9d8a48f20bdd81f5fbb1a38364cfaca73e1e41d1
mohammad-forhad-git <techwizard.forhad@gmail.com>
2025-05-19 11:05:37 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+# name="forhad"
+# print(name)
+# print("Hello "+ name)
+# apple="hello friend,\nthis is an apple "
+# print(apple)
+
+# st="""consectetur adipiscing elit. Etiam tristique ipsum quis luctus imperdiet. Aenean eu tempus nunc, vel interdum diam. Pellentesque venenatis leo a metus scelerisque, non convallis dolor finibus. Mauris augue ipsum, semper ut dui sit amet, pharetra fringilla ex. Aliquam sit amet nunc tincidunt, aliquet urna id, imperdiet nunc. Aenean id tortor lobortis"""
+# print(st)
+
+
+# # #print individual characters
+# # print(name[0])
+# # print(name[1])
+# # print(name[2])
+# # print(name[3])
+
+# for character in name:
+#     print(character)
+
+# #String slicing
+
+# fruit = "Mango"
+# mangolen=len(fruit)
+# print(mangolen)
+# # print(fruit[0:4])
+# # print(fruit[0:3])
+# print("len")
+# print(fruit[-1:5])#len(fruit)-1
+# # print(fruit[2:4])
+# # print(fruit[-2:5])
+for x in "mango":
+    print(x)
