1d059c4575ecfc12c3a05000dafb05d03cfa8b72
mohammad-forhad-git <techwizard.forhad@gmail.com>
2025-05-19 11:05:06 +0600

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,25 @@
+a=34#integer
+print(a)
+
+b = "python"#string
+print(b)
+
+#concatenate
+a1=9
+print(a+a1)
+c= True
+d= None
+
+print("The type od a is", type(a))
+print("The type od b is", type(b))
+print("The type od c is", type(c))
+print("The type od d is", type(d))
+
+list1 = [6,7.7,["apple"],[4,-4],["apple","i love banana"]]
+print(list1)#mutable
+
+tuple1 = (("parrot","sparrow"),("lion","tiger"))
+print(tuple1)#immutable
+
+dict = {"name":"pyhton","age":20,"canvot":True}
+print(dict)
