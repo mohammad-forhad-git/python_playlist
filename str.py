21b7b99743a8cf9b5a0ecf2214dd34c47e11bd6f
mohammad-forhad-git <techwizard.forhad@gmail.com>
2025-05-31 09:34:32 +0600

    Add files via upload

---
+++ str.py
@@ -0,0 +1,56 @@
+#learning-1
+
+# name="forhad"
+# print(name)
+# print("Hello "+ name)
+# apple="hello friend,\nthis is an apple "
+# print(apple)
+
+#learning-2
+# st="""consectetur adipiscing elit. Etiam tristique ipsum quis luctus imperdiet. Aenean eu tempus nunc, vel interdum diam. Pellentesque venenatis leo a metus scelerisque, non convallis dolor finibus. Mauris augue ipsum, semper ut dui sit amet, pharetra fringilla ex. Aliquam sit amet nunc tincidunt, aliquet urna id, imperdiet nunc. Aenean id tortor lobortis"""
+# print(st)
+
+
+# # #print individual characters
+# # print(name[0])
+# # print(name[1])
+# # print(name[2])
+# # print(name[3])
+
+# for character in name:
+#     print(character)
+
+
+#learning-3
+# #String slicing
+
+fruit = "Mango"
+# mangolen=len(fruit)
+# print(mangolen)
+# # print(fruit[0:4])
+# # print(fruit[0:3])
+#print("len")
+# print(fruit[-1:5])#len(fruit)-1
+# # print(fruit[2:4])
+# # print(fruit[-2:5])
+print(fruit[2:-2])
+
+
+#print all alphabets
+# for x in "mango":
+#     print(x)
+
+#learning-4
+#Check if "free" is present in the following text
+
+#txt="The best thing in life is free"
+# print("free" in text)#True
+
+
+# if "free" in text:
+#     print("Yes, 'free' is present.")
+
+# if "bes" in txt:
+#     print("Yes, 'best' is present there")
+# else:
+#     print("sorry , 'best' is not present there")
