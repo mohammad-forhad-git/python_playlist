5e49defa028d481ab94333526aa44cbd79ff20fb
mohammad-forhad-git <techwizard.forhad@gmail.com>
2025-05-31 09:35:11 +0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,53 @@
+# Addition=a+b
+# multiplication=a*b
+# substruction=a-b
+# Division=a/b
+# modulus =a%b 
+# exponential=a**b
+# floor division=a//b
+
+
+##calculator1
+# a=5
+# b=9
+
+
+# print("summation of a and b is:", a+b)
+# print("s
+# Substraction of a and b is:",a-b)
+# print("Multiplication of a and b:",a*b)
+# print("DIvision of a and b is:",a/b)
+# print("Modulus of and b is:",a%b)
+# print("floor division of a and b is:",a//b)
+# print("Exponential of a and b is:",a**b)
+
+# ##calculator2
+# #typecasting
+# a="3"
+# b="4"
+
+# print(int(a)+int(b))
+# #explicit typecasting
+# c=1.4
+# d=2.4
+# print(c+d)
+
+
+# #input_taking
+# #syntax  a=input()
+# a=input("Enter your name:")
+# print("My name is",a)
+
+# x=input("Enter your first number:")
+# y=input("enter your second number:")
+# print(int(x)+int(y)) ##typecasting
+
+# ##concatenate
+# m=input("First Name:")
+# n=input("Last Name:")
+
+# print(m+n)
+
+#Updated_CAlculator
+
+
